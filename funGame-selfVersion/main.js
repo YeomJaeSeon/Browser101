620baaf3d8d925de2c81de7495cfcfc421f66cb4
YeomJaeSeon <a89541457@gmail.com>
2020-10-12 01:42:29 +0900

    스스로 만드는게임 - 기본적인 구현은다함. sound아직 안넣음

--- funGame-selfVersion/main.js
+++ funGame-selfVersion/main.js
@@ -6,6 +6,7 @@ const count = document.querySelector(".count");
 const timer = document.querySelector(".timer");
 const alertBox = document.querySelector(".alert");
 const replayBtn = document.querySelector(".replayBtn");
+const message = document.querySelector("h1");
 
 let start; //setInterval clearInterval을 위한 전역 변수
 let timerCount = 10; //제한시간 전역변수로 설정
@@ -16,39 +17,62 @@ const timerFunction = () => {
     timer.innerText = `0:${timerCount}`;
     --timerCount;
     if (timerCount === -1) {
-      clearInterval(start);
-
+      message.innerText = "Time out!!!";
+      createAlertBox();
       timerCount = 10;
     }
   }, 1000);
 };
 
+//벌레 당근 재배치
+const rePosition = () => {
+  monsters.innerHTML = `
+  <img src="./img/bug.png" alt="" class="bug">
+  <img src="./img/bug.png" alt="" class="bug">
+  <img src="./img/bug.png" alt="" class="bug">
+  <img src="./img/bug.png" alt="" class="bug">
+  <img src="./img/bug.png" alt="" class="bug">
+  <img src="./img/bug.png" alt="" class="bug">
+  <img src="./img/bug.png" alt="" class="bug">
+  <img src="./img/carrot.png" alt="" class="carrot">
+  <img src="./img/carrot.png" alt="" class="carrot">
+  <img src="./img/carrot.png" alt="" class="carrot">
+  <img src="./img/carrot.png" alt="" class="carrot">
+  <img src="./img/carrot.png" alt="" class="carrot">
+  <img src="./img/carrot.png" alt="" class="carrot">
+  <img src="./img/carrot.png" alt="" class="carrot">
+  <img src="./img/carrot.png" alt="" class="carrot">
+  <img src="./img/carrot.png" alt="" class="carrot">      
+  <img src="./img/carrot.png" alt="" class="carrot">
+  `;
+  for (let i = 0; i < monsters.children.length; i++) {
+    // 벌레 당근은 x : 650px이내, y : 180px이내 존재해야함
+    let randomX = getRandomInt(0, 650);
+    let randomY = getRandomInt(0, 180);
+
+    // 벌레 당근 z-index가 달라야 게임할맛이 나지
+    let zIndex = getRandomInt(0, 3);
+
+    monsters.children[i].setAttribute(
+      "style",
+      `transform:translate(${randomX}px, ${randomY}px); z-index:${zIndex}`
+    );
+  }
+};
+
 // play 버튼 클릭
-playBtn.addEventListener("click", (e) => {
+playBtn.addEventListener("click", () => {
   // 재생버튼일 때
   if (playBtn.children[0].className === "fas fa-play playBtn__play") {
     timerFunction();
-    for (let i = 0; i < monsters.children.length; i++) {
-      monsters.children[i].classList.remove("invisible");
-      // 벌레 당근은 x : 650px이내, y : 180px이내 존재해야함
-      let randomX = getRandomInt(0, 650);
-      let randomY = getRandomInt(0, 180);
-
-      // 벌레 당근 z-index가 달라야 게임할맛이 나지
-      let zIndex = getRandomInt(0, 3);
-
-      monsters.children[i].setAttribute(
-        "style",
-        `transform:translate(${randomX}px, ${randomY}px); z-index:${zIndex}`
-      );
-    }
+    rePosition();
   }
 
   // stop 버튼 누름
   if (playBtn.children[0].className === "fas fa-square playBtn__stop") {
-    clearInterval(start);
     playBtn.setAttribute("style", "opacity : 0");
-    alertBox.classList.remove("invisible");
+    message.innerText = "STOP!!!";
+    createAlertBox();
   }
 
   // 처음 play button누르고 stop버튼으로 변함
@@ -56,10 +80,11 @@ playBtn.addEventListener("click", (e) => {
   playBtn.innerHTML = '<i class="fas fa-square playBtn__stop"></i>';
 });
 
-// 멈췄다가 다시 재생
+// replay 버튼 클릭(처음부터 다시재생임)
 replayBtn.addEventListener("click", () => {
-  alertBox.classList.add("invisible");
+  deleteAlertBox();
   playBtn.setAttribute("style", "opacity : 1");
+  rePosition();
   timerCount = 10;
   timerFunction();
 });
@@ -75,9 +100,14 @@ monsters.addEventListener("click", (event) => {
   if (event.target.className === "carrot") {
     event.target.remove();
     count.innerText = carrotCount();
+    if (carrotCount() === 0) {
+      message.innerText = "SUCCESS!!!";
+      createAlertBox();
+    }
   }
   if (event.target.className === "bug") {
-    event.target.remove();
+    message.innerText = "Oops,, Bug!!";
+    createAlertBox();
   }
 });
 
@@ -90,4 +120,10 @@ const carrotCount = () => {
 // alertBox 생성
 const createAlertBox = () => {
   alertBox.classList.remove("invisible");
+  clearInterval(start); //alertBox 생성되면 시간 멈춰야함
+};
+
+// alertBox삭제
+const deleteAlertBox = () => {
+  alertBox.classList.add("invisible");
 };
